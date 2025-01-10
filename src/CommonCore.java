9e851719d6281c9d67a86f95bb44c6402c2d3bd6
Szilágyi_Botond <115093938+SzilagyiBotond@users.noreply.github.com>
2025-01-11 01:08:23 +0200

    Add parameter finetune and viz

--- src/CommonCore.java
+++ src/CommonCore.java
@@ -4,11 +4,11 @@ public class CommonCore {
     public static final double maxHeight = 1.0e6;
     public static final double maxDepth = 1.0e6;
     public static final double maxBodyRadius = 10;
-    public static final double timeIncrement = 0.01;
+    public static final double timeIncrement = 0.4;
     public static final double angle = 1.0;
     public static final double defaultMass = 1.0;
-    public static final int bodyCount = 1000;
-    public static final int iterationCount = 500;
+    public static final int bodyCount = 500;
+    public static final int iterationCount = 5000;
 
 }
 
